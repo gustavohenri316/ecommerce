3eb13b2c2627132bd0a893c22ff0031d25305a72
gustavohenri316 <gustavohenri316@gmail.com>
2022-06-27 14:58:43 -0300

    mod

--- src/pages/ProductList/index.jsx
+++ src/pages/ProductList/index.jsx
@@ -33,9 +33,26 @@ export const ProductList = () => {
             <Option>Yellow</Option>
             <Option>Green</Option>
           </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
         </Filter>
         <Filter>
           <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option  selected>
+              Newest
+            </Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
         </Filter>
       </FilterContainer>
       <Products />
